cf04e5b24472058cd217b3cd3db1b383bf203d12
Oleh <depjoys@gmail.com>
2025-05-24 15:15:18 +0300

    JSON Encoding

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"errors"
 	"net/http"
 	"strconv"
@@ -17,4 +18,42 @@ func (app *application) readIDParam(r *http.Request) (int64, error) {
 	}
 
 	return id, nil
+}
+
+func (app *application) writeJSON(w http.ResponseWriter, status int, data any, headers http.Header) error {
+	/*  1) First way
+		Using json.Encoder in a single step creating and writing JSON.
+		But if JSON encoding fails you have to review headers and return an error response.
+
+		data := map[string]string{
+	        "hello": "world",
+	    }
+	    w.Header().Set("Content-Type", "application/json")
+	    err := json.NewEncoder(w).Encode(data)
+	    if err != nil {
+	        app.logger.Error(err.Error())
+	        http.Error(w, "The server encountered a problem and could not process your request", http.StatusInternalServerError)
+			return
+	    }
+
+	*/
+
+	// 2) Second way
+	js, err := json.Marshal(data)
+	if err != nil {
+		return err
+	}
+
+	js = append(js, '\n')
+
+	//maps.Insert(w.Header(), maps.All(headers))
+	for key, value := range headers {
+		w.Header()[key] = value
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	w.Write(js)
+
+	return nil
 }
