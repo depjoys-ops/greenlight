a517f3fb246570e3703b37886bce91e98d2b39da
Oleh Kuzmych <depjoys@gmail.com>
2025-06-07 08:48:55 +0000

    Sorting Lists

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"database/sql"
 	"errors"
+	"fmt"
 	"time"
 
 	"github.com/depjoys-ops/greenlight/internal/validator"
@@ -158,13 +159,15 @@ func (m MovieModel) Delete(id int64) error {
 // curl "localhost:4000/v1/movies?genres=western"
 // curl "localhost:4000/v1/movies?title=panther"
 // curl "localhost:4000/v1/movies?title=the+club"
+// curl "localhost:4000/v1/movies?sort=-title"
+// curl "localhost:4000/v1/movies?sort=-runtime"
 func (m MovieModel) GetAll(title string, genres []string, filters Filters) ([]*Movie, error) {
-	query := `
+	query := fmt.Sprintf(`
         SELECT id, created_at, title, year, runtime, genres, version
         FROM movies
         WHERE (to_tsvector('simple', title) @@ plainto_tsquery('simple', $1) OR $1 = '')
         AND (genres @> $2 OR $2 = '{}')
-        ORDER BY id`
+        ORDER BY %s %s, id ASC`, filters.sortColumn(), filters.sortDirection())
 
 	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
 	defer cancel()
