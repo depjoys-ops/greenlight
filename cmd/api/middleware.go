a4e18ae5a7e45daca5253a1ebe7c4f3fa8672c90
Oleh Kuzmych <depjoys@gmail.com>
2025-06-09 11:36:08 +0000

    Global Rate Limiting

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -3,7 +3,23 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"golang.org/x/time/rate"
 )
+
+// for i in {1..6}; do curl http://localhost:4000/v1/healthcheck; done
+func (app *application) rateLimit(next http.Handler) http.Handler {
+	limiter := rate.NewLimiter(2, 4)
+
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if !limiter.Allow() {
+			app.rateLimitExceededResponse(w, r)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
+}
 
 func (app *application) recoverPanic(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
